84999724c82be71721f3ff0fbd48787e75c465f5
joshualimzk <joshua_lim_zhe_kai@s2019.ssts.edu.sg>
2020-08-18 08:51:36 +0800

    Initial Commit

---
+++ Syren/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Syren
+//
+//  Created by Joshua Lim on 18/8/20.
+//  Copyright © 2020 Joshua Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
